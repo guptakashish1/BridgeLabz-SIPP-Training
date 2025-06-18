66b53e0c13803bde61d50d30232b9c60ae156d9d
guptakashish1 <160406127+guptakashish1@users.noreply.github.com>
2025-06-18 15:05:21 +0530

    Delete ProgrammingElements/LEVEL-1/HarryAge.java

--- ProgrammingElements/LEVEL-1/HarryAge.java
+++
@@ -1,8 +0,0 @@
-public class HarryAge{
- public static void main(String[] args){
-      int birthYear=2000;
-      int currentYear=2024;
-      int harryCurrentAge=currentYear-birthYear;
-      System.out.println("Harry's Age in 2024 is: "+harryCurrentAge);
-     }
-}
