fe26f6b7263820ef6a32fd0e362fe688a389ee30
guptakashish1 <160406127+guptakashish1@users.noreply.github.com>
2025-06-18 16:02:21 +0530

    [Kashish Gupta] Added Harry's Age

---
+++ ProgrammingElements/LEVEL-1/KilometerToMileConverter.java
@@ -0,0 +1,16 @@
+
+public class KilometerToMileConverter {
+
+    public static void main(String[] args) {
+        double distanceInKilometers = 10.8;
+        double conversionFactor = 1.6;
+
+        double distanceInMiles = convertToMiles(distanceInKilometers, conversionFactor);
+
+        System.out.println("The distance " + distanceInKilometers + " km in miles is " + distanceInMiles);
+    }
+
+    public static double convertToMiles(double kilometers, double factor) {
+        return kilometers * factor;
+    }
+}
